b11941cb93ab3e2f6598c93ec0e0938a985fae7d
youseop <youseop.lee@voyagerx.com>
2021-11-10 00:07:44 +0900

    settings for deploy

--- web.js
+++ web.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
